a29f41db63d3af7e4adf681b8a2db59ee0caafde
Solène Rambaut <rambaut.solene@yahoo.fr>
2019-05-11 16:16:43 +0200

    ajout de default pour quête symfony 3

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+// src/Controller/BlogController.php
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_index")
+     */
+    public function index()
+    {
+        return $this->render('default.html.twig');
+    }
+}
