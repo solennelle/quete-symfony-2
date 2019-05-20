f52d37bb5b24f672f5fad44bded19aa59da569e4
Solène Rambaut <rambaut.solene@yahoo.fr>
2019-05-20 09:38:11 +0200

    correction name symfony 9

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -81,7 +81,7 @@ class BlogController extends AbstractController
      *
      * @param string $category
      *
-     * @Route("blog/category/{id<\d+>}",
+     * @Route("blog/category/{name<^[a-z0-9-]+$>}",
      *     defaults={"slug" = null},
      *     name="category_show")
      * @return Response A response instance
